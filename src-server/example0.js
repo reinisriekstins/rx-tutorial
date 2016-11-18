21cecf1364436476283c8bca2c3acb2ffef6b73d
Reinis Riekstins <rr14022@lu.lv>
2016-11-18 17:51:28 +0200

    Section 3, Lecture 11 end

--- src-server/example0.js
+++ src-server/example0.js
@@ -1,5 +1,37 @@
 import moment from 'moment'
+import Rx from 'rxjs/Rx'
 
-console.log( moment().format() )
+/*---------------------------------
+ *
+ * Section 3, Lecture 11 end
+ *
+ *---------------------------------*/
 
-console.log('y, nig')
+const simple$ = new Rx.Observable(observer => {
+  console.log('generating observable')
+  setTimeout(() => {
+    observer.next('item')
+    setTimeout(() => {
+      observer.next('item2')
+      observer.complete()
+    }, 1000)
+  }, 1000)
+})
+
+simple$.subscribe(
+  item => console.log(`one.next: ${ item }`), // next
+  err => console.error(`one.error: ${ err }`), // error
+  () => console.log('complete') // complete
+)
+
+const error$ = new Rx.Observable(observer => {
+  observer.error(new Error('WHOA!!'))
+})
+
+setTimeout(() => {
+  error$.subscribe({
+    next: item => console.log(`two.next: ${ item }`), // next
+    error(err) { console.error(`two.error: ${ err.stack }`) }, // error
+    complete() { console.log('complete') } // complete
+  })
+}, 3000)
