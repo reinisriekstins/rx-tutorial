f00d7a4a5103a84e2684e73888d4c2a3cf2a159f
Reinis Riekstins <rr14022@lu.lv>
2016-11-18 17:51:15 +0200

    no changes worth mentioning here

--- src-client/example0.js
+++ src-client/example0.js
@@ -1,7 +1,7 @@
 import $ from 'jquery'
 import Rx from 'rxjs/Rx'
 
-const 
+const
   $title = $('#title'),
   $results = $('#results')
 
@@ -9,30 +9,33 @@ const getItems = title => {
   console.log( `Querying ${title}` )
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve([title, 'item2', `Another ${Math.random()}`])
-    }, 500 + (Math.random() * 200))
+      resolve([title, 'item2', `Another ${ Math.random() }`])
+    }, 500 + (Math.random() * 300))
   })
 }
 
 /*---------------------------------
  *
- * Reactive implementation:
+ * Reactive implementation
  *
  *---------------------------------*/
 
 Rx.Observable.fromEvent($title, 'keyup')
-.map(e => e.target.value)
+.map(e => {
+  $results.empty()
+  return e.target.value
+})
 .distinctUntilChanged()
-.debounceTime(500)
+.debounceTime(300)
 .switchMap(getItems)
 .subscribe(items => {
   $results.empty()
   $results.append(items.map(i => $(`<li />`).text(i)))
 })
 
-/*---------------------------------
+/*---------------------------------*
  *
- * Traditional implementation:
+ * Traditional implementation
  *
  *---------------------------------*/
 
