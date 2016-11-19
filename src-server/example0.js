185830ef4072b8d113e596690c27aab70973fcea
Reinis Riekstins <rr14022@lu.lv>
2016-11-19 12:41:34 +0200

    not sure

--- src-server/example0.js
+++ src-server/example0.js
@@ -3,35 +3,94 @@ import Rx from 'rxjs/Rx'
 
 /*---------------------------------
  *
- * Section 3, Lecture 11 end
+ * Section 3, Lecture 11, start
  *
  *---------------------------------*/
 
-const simple$ = new Rx.Observable(observer => {
-  console.log('generating observable')
-  setTimeout(() => {
-    observer.next('item')
-    setTimeout(() => {
-      observer.next('item2')
-      observer.complete()
-    }, 1000)
-  }, 1000)
-})
+// const simple$ = new Rx.Observable(observer => {
+//   console.log('generating observable')
+//   setTimeout(() => {
+//     observer.next('item')
+//     setTimeout(() => {
+//       observer.next('item2')
+//       observer.complete()
+//     }, 1000)
+//   }, 1000)
+// })
 
-simple$.subscribe(
-  item => console.log(`one.next: ${ item }`), // next
-  err => console.error(`one.error: ${ err }`), // error
-  () => console.log('complete') // complete
-)
+// simple$.subscribe(
+//   item => console.log(`one.next: ${ item }`), // next
+//   err => console.error(`one.error: ${ err }`), // error
+//   () => console.log('complete') // complete
+// )
 
-const error$ = new Rx.Observable(observer => {
-  observer.error(new Error('WHOA!!'))
-})
+// const error$ = new Rx.Observable(observer => {
+//   observer.error(new Error('WHOA!!'))
+// })
 
-setTimeout(() => {
-  error$.subscribe({
-    next: item => console.log(`two.next: ${ item }`), // next
-    error(err) { console.error(`two.error: ${ err.stack }`) }, // error
-    complete() { console.log('complete') } // complete
-  })
-}, 3000)
+// setTimeout(() => {
+//   error$.subscribe({
+//     next: item => console.log(`two.next: ${ item }`), // next
+//     error(err) { console.error(`two.error: ${ err.stack }`) }, // error
+//     complete() { console.log('complete') } // complete
+//   })
+// }, 3000)
+
+/*---------------------------------
+ *
+ * Section 3, Lecture 11, time: 11:40 - Lecture 12, end
+ *
+ *---------------------------------*/
+
+// function createInterval$ (time) {
+//   return new Rx.Observable(observer => {
+//     let index = 0
+//     let interval = setInterval(() => {
+//       console.log(`generating ${ index }`)
+//       observer.next(index++)
+//     }, time)
+
+//     return () => {
+//       clearInterval(interval)
+//     }
+//   })
+// }
+
+// function createSubscriber(tag) {
+//   return {
+//     next(item) {
+//       console.log(`${ tag }.next ${ item }`)
+//     },
+//     error(err) {
+//       console.log(`${ tag }.error ${ err.stack }`)
+//     },
+//     complete() {
+//       console.log(`${ tag }.complete`)
+//     }
+//   }
+// }
+
+// function take$(sourceObservable$, amount) {
+//   return new Rx.Observable(observer => {
+//     let count = 0;
+//     const subscription = sourceObservable$.subscribe({
+//       next(item) {
+//         observer.next(item)
+//         if (++count >= amount) observer.complete()
+//       },
+//       error(err) { observer.error(err) },
+//       complete() { observer.complete() }
+//     })
+
+//     return () => subscription.unsubscribe()
+//   })
+// }
+
+// const everySecond$ = createInterval$(1000)
+// const firstFiveSeconds$ = take$(everySecond$, 5)
+// const subscription = firstFiveSeconds$.subscribe(createSubscriber('one'))
+
+// setTimeout(() => {
+//   subscription.unsubscribe()
+// }, 3500)
+
